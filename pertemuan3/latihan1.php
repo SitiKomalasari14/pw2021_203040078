40d3713b6852b74dde0f89c7c21607543dd8808c
SitiKomalasari14 <sitikomalasari1417@gmail.com>
2021-02-19 09:20:45 +0700

    menambahkan pertemuan3

---
+++ pertemuan3/latihan1.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Siti Komalasari
+203040078
+github.com/SitiKomalasari14/pw2021_203040078
+Pertemuan 3 - (17 Februari 2021)
+Materi pertemuan ke-3 ini akan mempelajari mengenai Pengulangan dan Pengkondisian 
+*/
+?>
+
+<?php 
+// PENGULANGAN
+// for
+// while
+// do.. while 
+// foreach : khusus untuk array 
+
+for($i = 0; $i < 5; $i++) {
+    echo "Hello Wolrd!  ";
+}
+
+for($i = 0; $i < 5; $i++) {
+    echo "Hello Wolrd <br> ";
+}
+
+$i = 0;
+while($i < 5) {
+    echo "Hello World! <br>";
+$i++;
+}      
+
+$i = 0;
+do {
+    echo "Hello World! <br>";
+$i++;
+} while ( $i < 5);
+
+$i = 10;
+do {
+    echo "Hello World! <br>";
+$i++;
+} while ( $i < 5);
+
+?>
+
+
+
+
---
+++ pertemuan3/latihan2.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Siti Komalasari
+203040078
+github.com/SitiKomalasari14/pw2021_203040078
+Pertemuan 3 - (17 Februari 2021)
+Materi pertemuan ke-3 ini akan mempelajari mengenai Pengulangan dan Pengkondisian 
+*/
+?>
+
+<?php 
+// PENGKONDISIAN / PERCABANGAN
+// if else
+// if else if else
+// ternary  =====> untuk mengganti if else yang sederhana
+// switch =======> digunakan jika sdh terlalu banyak membuat if else
+
+$x = 10;
+if($x < 20) {
+    echo "benar";
+}
+
+$x = 20;
+if($x < 20) {
+    echo "benar";
+}
+
+$x = 10;
+if($x < 20) {
+    echo "benar";
+} else {
+    echo "salah";
+}
+
+$x = 20; 
+if( $x < 20 ) {
+    echo "benar";
+} else if( $x == 20) {
+    echo "bingo!";
+} else {
+    echo "salah";
+}
+
+$x = 30; 
+if( $x < 20 ) {
+    echo "benar";
+} else if( $x == 20) {
+    echo "bingo!";
+} else {
+    echo "salah";
+}
+
+$x = 10; 
+if( $x < 20 ) {
+    echo "benar";
+} else if( $x == 20) {
+    echo "bingo!";
+} else {
+    echo "salah";
+}
+
+
+
+?>
