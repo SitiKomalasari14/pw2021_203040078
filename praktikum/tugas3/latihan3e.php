0bce14a6eb811efa68dabc466a99c27d283f4fc5
SitiKomalasari14 <sitikomalasari1417@gmail.com>
2021-03-16 22:53:25 +0700

    praktikum pertemuan3

--- praktikum/tugas3/latihan3e.php
+++ praktikum/tugas3/latihan3e.php
@@ -96,8 +96,8 @@ $buku = [
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css"
+    rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <title>BOOK_ID</title>
   </head>
   <body>
@@ -130,5 +130,6 @@ $buku = [
         <?php endforeach; ?>
     </tbody>
   </div>
+ </body>
 </table>
 </html>
